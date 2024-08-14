c9b9d68e2d19fe10ae67537538de4dfbac511aa0
Isas321 <javier.isas321@gmail.com>
2024-08-14 01:03:04 -0300

    Menu para elegir entre Calculadora y administracion de empleados

--- Program.cs
+++ Program.cs
@@ -1,72 +1,120 @@
-﻿using EspacioCalculadora;
+﻿using System;
+using EspacioCalculadora;
 //Enu define un dominio, define los tio de dato
-Calculadora calcula = new();  
-int opcion;
-double x, valorAntesDeOperar;
 
-do
-{
-    Console.WriteLine("\n\n\nCalculadora\n");
-    Console.WriteLine("1-Sumar");
-    Console.WriteLine("2-Restar");
-    Console.WriteLine("3-Multiplicar");
-    Console.WriteLine("4-Dividir");
-    Console.WriteLine("5-Limpiar");
-    Console.WriteLine("Salir -> Presione cualquier otra tecla");
-    Console.Write("\nIngrese opcion: ");
-    opcion = ingresaEntero();
-    switch (opcion)
+class Program{
+  static void Main(){
+    int opcionPrincipal;
+    do
     {
-        case 1:
-            valorAntesDeOperar=calcula.Resultado;
-            Console.Write("\nValor actual: "+valorAntesDeOperar);
-            Console.Write("\nIngrese numero que desea sumar: ");
-            x = ingresarNumeroReal();
-            calcula.Sumar(x);
-            Console.WriteLine("\nEl resultado de "+valorAntesDeOperar+" + "+x+" es: "+calcula.Resultado);
-            Console.Write("\nPrecione una tecla para continuar\t");
-            Console.ReadKey();
-            break;
-        case 2:
-            valorAntesDeOperar=calcula.Resultado;
-            Console.Write("\nValor actual: "+valorAntesDeOperar);
-            Console.Write("\nIngrese numero que desea restar: ");
-            x = ingresarNumeroReal();
-            calcula.Restar(x);
-            Console.WriteLine("\nEl resultado de "+valorAntesDeOperar+" - "+x+" es: "+calcula.Resultado);
-            Console.Write("\nPrecione una tecla para continuar\t");
-            Console.ReadKey();
-        break;
-      case 3:
-            valorAntesDeOperar=calcula.Resultado;
-            Console.Write("\nValor actual: "+valorAntesDeOperar);
-            Console.Write("\nIngrese numero por el que desea multiplicar: ");
-            x = ingresarNumeroReal();
-            calcula.Multiplicar(x);
-            Console.WriteLine("\nEl resultado de "+valorAntesDeOperar+" * "+x+" es: "+calcula.Resultado);
-            Console.Write("\nPrecione una tecla para continuar\t");
-            Console.ReadKey();
-        break;
-      case 4:
-            valorAntesDeOperar=calcula.Resultado;
-            Console.Write("\nValor actual: "+valorAntesDeOperar);
-            Console.Write("\nIngrese numero por el que desea dividir: ");
-            do{
-              x = ingresarNumeroReal();
-            } while(x==0);
-            calcula.Dividir(x);
-            Console.WriteLine("\nEl resultado de "+valorAntesDeOperar+" / "+x+" es: "+calcula.Resultado);
-            Console.Write("\nPrecione una tecla para continuar\t");
-            Console.ReadKey();
-        break;
-      default:
-        Console.WriteLine("\nSaliendo del programa...");
-        return 0; //Termina el programa
-    }
-} while (opcion==1 || opcion==2 || opcion==3 || opcion==4);
-return 0;
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1. Calculadora");
+        Console.WriteLine("2. Administracion de empleados");
+        Console.WriteLine("0. Salir");
 
-int ingresaEntero(){
+        Console.Write("Seleccione una opción: ");
+        opcionPrincipal = int.Parse(Console.ReadLine());
+
+        switch (opcionPrincipal)
+        {
+            case 1:
+                Console.WriteLine("Has seleccionado Calculadora.");
+                CalculadoraBasica();
+                break;
+
+            case 2:
+                Console.WriteLine("Has seleccionado Administracion de empleados.");
+                break;
+            
+            case 0:
+                Console.WriteLine("Saliendo del programa...");
+                break;
+            
+            default:
+                Console.WriteLine("Opción no válida. Por favor, seleccione una opción válida.");
+                break;
+        }
+
+        if (opcionPrincipal != 0)
+        {
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+
+    } while (opcionPrincipal != 0); // Repetir hasta que el usuario elija salir      
+  }
+
+  static void CalculadoraBasica()
+  {
+      Calculadora calcula = new();
+      int opcion;
+      double x, valorAntesDeOperar;
+      do
+      {
+
+          Console.WriteLine("\n\n\nCalculadora\n");
+          Console.WriteLine("1-Sumar");
+          Console.WriteLine("2-Restar");
+          Console.WriteLine("3-Multiplicar");
+          Console.WriteLine("4-Dividir");
+          Console.WriteLine("5-Limpiar");
+          Console.WriteLine("Salir -> Presione cualquier otra tecla");
+          Console.Write("\nIngrese opcion: ");
+          opcion = ingresaEntero();
+          switch (opcion)
+          {
+              case 1:
+                  valorAntesDeOperar = calcula.Resultado;
+                  Console.Write("\nValor actual: " + valorAntesDeOperar);
+                  Console.Write("\nIngrese numero que desea sumar: ");
+                  x = ingresarNumeroReal();
+                  calcula.Sumar(x);
+                  Console.WriteLine("\nEl resultado de " + valorAntesDeOperar + " + " + x + " es: " + calcula.Resultado);
+                  Console.Write("\nPrecione una tecla para continuar\t");
+                  Console.ReadKey();
+                  break;
+              case 2:
+                  valorAntesDeOperar = calcula.Resultado;
+                  Console.Write("\nValor actual: " + valorAntesDeOperar);
+                  Console.Write("\nIngrese numero que desea restar: ");
+                  x = ingresarNumeroReal();
+                  calcula.Restar(x);
+                  Console.WriteLine("\nEl resultado de " + valorAntesDeOperar + " - " + x + " es: " + calcula.Resultado);
+                  Console.Write("\nPrecione una tecla para continuar\t");
+                  Console.ReadKey();
+                  break;
+              case 3:
+                  valorAntesDeOperar = calcula.Resultado;
+                  Console.Write("\nValor actual: " + valorAntesDeOperar);
+                  Console.Write("\nIngrese numero por el que desea multiplicar: ");
+                  x = ingresarNumeroReal();
+                  calcula.Multiplicar(x);
+                  Console.WriteLine("\nEl resultado de " + valorAntesDeOperar + " * " + x + " es: " + calcula.Resultado);
+                  Console.Write("\nPrecione una tecla para continuar\t");
+                  Console.ReadKey();
+                  break;
+              case 4:
+                  valorAntesDeOperar = calcula.Resultado;
+                  Console.Write("\nValor actual: " + valorAntesDeOperar);
+                  Console.Write("\nIngrese numero por el que desea dividir: ");
+                  do
+                  {
+                      x = ingresarNumeroReal();
+                  } while (x == 0);
+                  calcula.Dividir(x);
+                  Console.WriteLine("\nEl resultado de " + valorAntesDeOperar + " / " + x + " es: " + calcula.Resultado);
+                  Console.Write("\nPrecione una tecla para continuar\t");
+                  Console.ReadKey();
+                  break;
+              case 5:
+                  Console.WriteLine("\nSaliendo del programa...");
+                  break;
+          }
+      } while (opcion == 1 || opcion == 2 || opcion == 3 || opcion == 4);
+  }
+
+
+  static int ingresaEntero(){
   int num;
   if(int.TryParse(Console.ReadLine(), out num)){
     //int.Tryparse -> devuelve un booleano, si es que se puede converttir da true
@@ -76,7 +124,8 @@ int ingresaEntero(){
   }
 }
 
-double ingresaReal(){
+
+  static double ingresaReal(){
   double num;
   if(double.TryParse(Console.ReadLine(), out num)){
     return num;
@@ -85,12 +134,13 @@ double ingresaReal(){
   }
 }
 
-double ingresarNumeroReal()
-{
-    double a;
-    do
-    {
-        a = ingresaReal();
-    } while (a == -999);
-    return a;
+  static double ingresarNumeroReal()
+  {
+      double a;
+      do
+      {
+          a = ingresaReal();
+      } while (a == -999);
+      return a;
+  }
 }
